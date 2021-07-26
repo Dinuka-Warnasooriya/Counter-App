97e41803e30bbbf5cceca6fc88e2f06648ce75af
Dinuka-Sandaruwan-Warnasooriya <dinukaswarnasooriya@gmail.com>
2021-07-27 00:23:35 +0530

    Using object destructureing

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,14 +1,22 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
-    state = {  }
+    state = {  
+        count:0,
+    };
     render() { 
         return (
             <div>
-                <h1>Hello world</h1>
+                <span>{this.formatCount()}</span>
+                <button>Increment</button>
             </div>
 
           );
+    }
+
+    formatCount(){
+        const {count} = this.state;
+        return count === 0 ? 'Zero' : count;
     }
 }
  
